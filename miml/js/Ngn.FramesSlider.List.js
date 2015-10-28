82e4a626af92e63204077453d418b3c331698356
masted <masted311@gmail.com>
2015-10-28 03:32:13 +0300

    ...

--- miml/js/Ngn.FramesSlider.List.js
+++ miml/js/Ngn.FramesSlider.List.js
@@ -2,7 +2,7 @@ Ngn.FramesSlider.List = new Class({
   Extends: Ngn.FramesSlider,
   currentItemId: 0,
   getScrollKey: function() {
-    if (!this.currentItemId) throw new Error('Set data-id attribute to item elements');
-    return this.frameN + this.currentItemId;
+    if (this.frameN == 2 && !this.currentItemId) throw new Error('Set data-id attribute to item elements');
+    return this.frameN + (this.currentItemId || '');
   }
 });
